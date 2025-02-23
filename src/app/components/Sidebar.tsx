86e748db45298231a5fcc8e219db47481c7f95d1
Danjuma kuti <ayanfolarin@gmail.com>
2025-02-23 18:30:55 +0100

    done

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -11,7 +11,6 @@ import { GrHomeRounded } from "react-icons/gr";
 import { usePathname } from "next/navigation";
 import { CiLogin } from "react-icons/ci";
 
-
 const Sidebar = () => {
   const router = useRouter();
   const pathname = usePathname();
@@ -34,88 +33,86 @@ const Sidebar = () => {
 
       {/* Sidebar */}
       <aside
-  className={`fixed lg:relative h-screen w-64 transform transition-transform duration-300 ease-in-out border-r-2 flex flex-col 
-    ${isOpen ? "translate-x-0" : "-translate-x-full"} lg:translate-x-0 bg-white z-20 overflow-y-auto`}
->
+        className={`fixed lg:relative h-screen w-64 transform transition-transform duration-300 ease-in-out border-r-2 flex flex-col 
+    ${
+      isOpen ? "translate-x-0" : "-translate-x-full"
+    } lg:translate-x-0 bg-white z-20 overflow-y-auto`}
+      >
+        <Image
+          src={logo}
+          alt="logo"
+          width={138}
+          height={45}
+          // layout="responsive"
+          className="my-5 object-contain"
+          style={{ objectFit: "contain" }}
+        />
 
-
-  <Image
-    src={logo}
-    alt="logo"
-    width={138}
-    height={45}
-    // layout="responsive"
-    className="my-5 object-contain"
-    style={{ objectFit: "contain" }}
-
-  />
-
-  <div className="p-4 flex-grow">
-    <p
-      onClick={() => router.push("/")}
-      className={`rounded-sm font-medium text-xs px-3 py-2 cursor-pointer flex items-center gap-3 hover:bg-[#F1F7FF] ${
-        pathname === "/" ? "text-[#014DAF]" : "text-[#808080]"
-      }`}
-    >
-      <GrHomeRounded
-        className={pathname === "/" ? "text-[#014DAF]" : "text-[#808080]"}
-      />
-      Dashboard
-    </p>
-    <h2 className="font-medium text-[8px] text-[#7E8B9C] ml-3 mb-3 mt-2">
-      MAIN MENU
-    </h2>
-    <ul>
-      {generalDashboard.map((item, index) => (
-        <li key={index} className="mb-2">
-          <a
-            href={item.href}
-            className={`flex items-center p-2 hover:bg-[#F1F7FF] rounded transition-colors ${
-              location.pathname === item.href ? "bg-[#F6F6F6]" : ""
+        <div className="p-4 flex-grow">
+          <p
+            onClick={() => router.push("/")}
+            className={`rounded-sm font-medium text-xs px-3 py-2 cursor-pointer flex items-center gap-3 hover:bg-[#F1F7FF] ${
+              pathname === "/" ? "text-[#014DAF]" : "text-[#808080]"
             }`}
           >
-            {item.icon &&
-              (location.pathname === item.href ? (
-                <item.boldIcon className="w-4 h-4 mr-3 text-[#014DAF]" />
-              ) : (
-                <item.icon className="w-4 h-4 mr-3 text-[#808080]" />
-              ))}
+            <GrHomeRounded
+              className={pathname === "/" ? "text-[#014DAF]" : "text-[#808080]"}
+            />
+            Dashboard
+          </p>
+          <h2 className="font-medium text-[8px] text-[#7E8B9C] ml-3 mb-3 mt-2">
+            MAIN MENU
+          </h2>
+          <ul>
+            {generalDashboard.map((item, index) => (
+              <li key={index} className="mb-2">
+                <a
+                  href={item.href}
+                  className={`flex items-center p-2 hover:bg-[#F1F7FF] rounded transition-colors ${
+                    pathname === item.href ? "bg-[#F6F6F6]" : ""
+                  }`}
+                >
+                  {item.icon &&
+                    (location.pathname === item.href ? (
+                      <item.boldIcon className="w-4 h-4 mr-3 text-[#014DAF]" />
+                    ) : (
+                      <item.icon className="w-4 h-4 mr-3 text-[#808080]" />
+                    ))}
 
-            <span className="font-normal text-xs text-[#808080]"
-            >
-              {item.label}
-            </span>
-          </a>
-        </li>
-      ))}
-    </ul>
-  </div>
+                  <span className="font-normal text-xs text-[#808080]">
+                    {item.label}
+                  </span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-  {/* Move this div to the bottom */}
-  <div className="p-4 mt-auto">
-    <p className="font-medium text-xs p-1 flex items-center gap-3 mb-8">
-      <CiLogin className="text-xl" /> Logout
-    </p>
-    <p className="text-[8px] font-medium text-[#808080] p-2">POWERED BY</p>
-    <Image
-      src={cadinfra}
-      alt="cadinfra"
-      width={93}
-      height={43}
-      className="p-2 object-contain"
-      style={{ objectFit: "contain" }}
-    />
-  </div>
-</aside>
+        {/* Move this div to the bottom */}
+        <div className="p-4 mt-auto">
+          <p className="font-medium text-xs p-1 flex items-center gap-3 mb-8">
+            <CiLogin className="text-xl" /> Logout
+          </p>
+          <p className="text-[8px] font-medium text-[#808080] p-2">
+            POWERED BY
+          </p>
+          <Image
+            src={cadinfra}
+            alt="cadinfra"
+            width={93}
+            height={43}
+            className="p-2 object-contain"
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+      </aside>
 
-
-{isOpen && (
-  <div
-    onClick={toggleSidebar}
-    className="lg:hidden fixed inset-0 bg-black bg-opacity-50 z-10"
-  ></div>
-)}
-
+      {isOpen && (
+        <div
+          onClick={toggleSidebar}
+          className="lg:hidden fixed inset-0 bg-black bg-opacity-50 z-10"
+        ></div>
+      )}
     </>
   );
 };
