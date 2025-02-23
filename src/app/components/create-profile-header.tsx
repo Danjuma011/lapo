db43c6386589dcc7393053c8b447c9c90b6f105e
Danjuma kuti <ayanfolarin@gmail.com>
2025-02-23 18:13:09 +0100

    fix: router

--- src/app/components/create-profile-header.tsx
+++ src/app/components/create-profile-header.tsx
@@ -5,36 +5,38 @@ import img from "@public/svg/credit-card-pos.svg";
 import { PiBellLight } from "react-icons/pi";
 import GoBack from "@/component/back";
 import { RiArrowRightSLine } from "react-icons/ri";
-
+import { useRouter } from "next/navigation";
 
 const CreateProfileHeader = () => {
+  const router = useRouter();
+
   return (
     <header className="border border-b-2  p-4 bg-[#fefefe] ">
       <div className="flex justify-between">
-
         <div className="flex items-center gap-3">
-        <GoBack/>
-        <Image
-           src={img}
-           alt="manageCard"
-           width={20}
-           height={20}
-           className="mb-1"
-         />
-         <RiArrowRightSLine className="text-[#D0D5DD]"/>
+          <GoBack />
+          <Image
+            src={img}
+            alt="manageCard"
+            width={20}
+            height={20}
+            className="mb-1"
+          />
+          <RiArrowRightSLine className="text-[#D0D5DD]" />
           <p
-           onClick={() => window.history.back()}
-          className="text-xs font-medium cursor-pointer">Card Profile</p>
-<RiArrowRightSLine className="text-[#D0D5DD]"/>
+            onClick={() => router.back()}
+            className="text-xs font-medium cursor-pointer"
+          >
+            Card Profile
+          </p>
+          <RiArrowRightSLine className="text-[#D0D5DD]" />
           <p className="text-xs font-bold">Create Profile</p>
-        
         </div>
         <div className="flex items-center gap-3">
-        
-          <PiBellLight size={20}/>
+          <PiBellLight size={20} />
           <div className="bg-[#F2F4F7] rounded-full">
-  <FiUser size={30} className="p-2" />
-</div>
+            <FiUser size={30} className="p-2" />
+          </div>
         </div>
       </div>
     </header>
