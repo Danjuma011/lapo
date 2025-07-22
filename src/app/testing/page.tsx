9a2fb79d563e9f489463e11aad082b24998d4469
Danjuma kuti <ayanfolarin@gmail.com>
2025-07-22 13:24:38 +0100

    done

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,17 +1,8 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Page = () => {
-  // for (let i = 1; i <= 50; i += 1) {
-  //   if (i % 2 === 0) {
-  //     console.log(i + " is even");
-  //   } else console.log(i + " is odd");
-  //   if (i === 30) {
-  //     console.log(i + " is complete");
-  //   }
-  // }
-
   const [color, setColor] = useState("bg-red-500"); // Start with red
   const [startButton, setstartButton] = useState(false);
 
@@ -24,8 +15,9 @@ const Page = () => {
     "bg-red-500", // Adding red back to complete the cycle
   ];
 
-  useEffect(() => {
-    if (!startButton) return;
+  const startColor = () => {
+    setstartButton(true);
+
     let currentIndex = 0;
 
     const intervalId = setInterval(() => {
@@ -35,7 +27,7 @@ const Page = () => {
 
     // Clean up the interval when the component unmounts
     return () => clearInterval(intervalId);
-  }, [startButton]); // Empty dependency array means this runs once on mount
+  };
 
   return (
     <>
@@ -44,15 +36,7 @@ const Page = () => {
           className={`w-20 h-20 rounded-lg transition-colors duration-1000 ${color}`}
         ></div>
 
-        <button
-          onClick={() => {
-            setstartButton(!startButton);
-            // or
-            // setstartButton((prev) => !prev);
-          }}
-        >
-          {startButton ? "start" : "stop"}
-        </button>
+        <button onClick={startColor}>{startButton ? "start" : "stop"}</button>
       </div>
     </>
   );
--- src/app/utils/helper.ts
+++ src/app/utils/helper.ts
@@ -1,13 +1,8 @@
-type DropdownItem = {
-  label: string;
-  value: string | number;
-};
-
-export function convertEnumToDropdownItem(enumObj: object): DropdownItem[] {
-  return Object.entries(enumObj)
-    .filter(([key, value]) => isNaN(Number(key))) // filter out reverse mappings (for numeric enums)
-    .map(([key, value]) => ({
-      label: key.charAt(0).toUpperCase() + key.slice(1), // Capitalize label
-      value: value,
-    }));
+export function convertEnumToDropdownItem(
+  enumObj: object
+): { name: string; id: string }[] {
+  return Object.entries(enumObj).map(([key, value]) => ({
+    name: key.charAt(0).toUpperCase() + key.slice(1),
+    id: String(value),
+  }));
 }
